fd74d4aeb8196bb554a9734e198121fb5277592e
Gergely Laborci <gergely@laborci.hu>
2021-09-07 12:43:31 +0200

    minor fix

--- src/form/api.ts
+++ src/form/api.ts
@@ -12,13 +12,13 @@ export default class Api implements IFormApi {
 		return fetch(this.apiBase + "/blank", {method: "POST"}).then(handleFetch);
 	}
 	create(item: any): Promise<false | null | number> {
-		return fetch(this.apiBase + "/create/" + item.id, {method: "POST", body: JSON.stringify({item})}).then(handleFetch);
+		return fetch(this.apiBase + "/create/", {method: "POST", body: JSON.stringify({item})}).then(handleFetch);
 	}
 	update(item: any): Promise<false | null | number> {
 		return fetch(this.apiBase + "/update/" + item.id, {method: "POST", body: JSON.stringify({item})}).then(handleFetch);
 	}
 	delete(id: number): Promise<boolean> {
-		return fetch(this.apiBase + "/delete/" + id, {method: "POST"}).then(handleFetch).then(() => true);
+		return fetch(this.apiBase + "/delete/" + id, {method: "POST"}).then(handleFetch);
 	}
 
 }
